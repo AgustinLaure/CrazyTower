9ddca6fc0feacfe6c3fe1ca778fd3fdf1424122c
AgustinLaure <agustin.laure@estudiante.imagecampus.edu.ar>
2026-05-05 02:04:35 -0300

    Add Crane physics

--- Assets/Scripts/Crane.cs
+++ Assets/Scripts/Crane.cs
@@ -1,15 +1,44 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Crane : MonoBehaviour
 {
+    private const float epsilon = 1e-5f;
     [SerializeField] private float speed;
-    void Start()
+    [SerializeField] private Rigidbody rb;
+    private bool isSwinging;
+    private int pendulumPushDir = 1;
+    private void Update()
     {
-        
+
     }
 
-    void Update()
+    private void FixedUpdate()
     {
-        transform.Rotate(new Vector3 (0,0,1f), speed * Time.deltaTime);    
+        Swing();
+        if (Input.GetKey(KeyCode.K))
+        {
+            PushPendulum();
+        }
+
+    }
+
+    private void PushPendulum()
+    {
+        rb.AddRelativeForce(Vector3.right * speed, ForceMode.Acceleration);
+        Debug.Log("lineal" + rb.linearVelocity);
+        Debug.Log(" angular" + rb.angularVelocity);
+        isSwinging = true;
+    }
+    private void Swing()
+    {
+        if (isSwinging)
+        {
+           //if (!(rb.linearVelocity.x < 0 + epsilon && pendulumPushDir < 0 + epsilon))
+           //{
+           //    rb.AddRelativeForce(Vector3.right * (speed * pendulumPushDir), ForceMode.Acceleration);
+           //    pendulumPushDir *= -1;
+           //}
+        }
     }
 }
